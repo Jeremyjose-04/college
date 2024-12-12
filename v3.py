d2e90cf62a9bdb833cf8b650cd4f5d2d0a07b50b
Jeremyjose-04 <josejeremy04@gmail.com>
2024-12-12 21:29:40 +0530

    Create v3.py
    version 3 with file

---
+++ v3.py
@@ -0,0 +1,7 @@
+    a = int(file.readline().strip())
+    b = int(file.readline().strip())
+    c = int(file.readline().strip())
+    x = int(file.readline().strip())
+
+y = (a * (x ** 2)) + (b * x) + c
+print(f"Value of y: {y}")
