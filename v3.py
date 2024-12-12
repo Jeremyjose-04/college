75647a5c070cb1ed5086d3f64466bbb92447a974
Jeremyjose-04 <josejeremy04@gmail.com>
2024-12-12 21:32:46 +0530

    Update v3.py

--- v3.py
+++ v3.py
@@ -1,7 +1,9 @@
+with open('input.txt.txt', 'r') as file:
     a = int(file.readline().strip())
     b = int(file.readline().strip())
     c = int(file.readline().strip())
     x = int(file.readline().strip())
 
+# Calculate y
 y = (a * (x ** 2)) + (b * x) + c
 print(f"Value of y: {y}")
